8cd3aaa4042d67d777dc3ec00737b13a221ebe8a
dhelms33 <derrickhelms10@gmail.com>
2024-09-18 18:52:35 -0600

    added ruby code

--- 2ndproblem.rb
+++ 2ndproblem.rb
@@ -1,4 +1,4 @@
 require 'date'
 current_time = DateTime.now
 cdt = current_time.strft.time "%d%m%Y %H: %M"
-puts" Curretn date and time: "+CDT
+puts" Current date and time: "+CDT
---
+++ 3rdproblem.rb
@@ -0,0 +1,8 @@
+def new_str(n,str)
+  return str * n
+print new_str(1,a), "\n"
+print new_str(2,a), "\n"
+print new_str(3,a), "\n"
+print new_str(4,a), "\n"
+print new_str(5,a), "\n"
+
